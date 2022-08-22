2041c4d6ef753f8c3f43bfdadf759d0b3d6e7566
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-08-23 00:20:48 +0100

    Initial commit

---
+++ Simulator/OpenFile.xaml.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+
+namespace Simulator
+{
+	/// <summary>
+	/// Interaction logic for OpenFile.xaml
+	/// </summary>
+	public partial class OpenFile
+	{
+		public OpenFile()
+		{
+			InitializeComponent();
+			Messenger.Default.Register<NotificationMessage>(this, NotificationMessageReceived);
+		}
+
+		private void NotificationMessageReceived(NotificationMessage notificationMessage)
+		{
+			if (notificationMessage.Notification == "CloseFileWindow")
+				Close();
+		}
+	}
+}
