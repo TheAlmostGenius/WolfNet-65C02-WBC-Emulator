e298e098f8e341e6df1f2b66eafc811ba55ef837
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-09-12 03:21:29 +0100

    Update Hardware.cs

--- Hardware/Hardware.cs
+++ Hardware/Hardware.cs
@@ -8,6 +8,7 @@ namespace Hardware
         #region Fields
         public static string SettingsFile = "./Settings.xml";
         public static string ErrorFile = "./Errors.log";
+        public static string BiosFile = "./BIOS.bin";
         #endregion
 
         #region Properties
