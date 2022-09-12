7dcb20385d57c98fd99c06c962c05cd76bddedf4
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-09-12 03:21:34 +0100

    Update AssemblyFileModel.cs

--- Emulator/Model/AssemblyFileModel.cs
+++ Emulator/Model/AssemblyFileModel.cs
@@ -8,17 +8,7 @@
         /// <summary>
         /// The Program Converted into Hex.
         /// </summary>
-        public byte[] Bios { get; set; }
-
-        /// <summary>
-        /// The Program Converted into Hex.
-        /// </summary>
         public byte[] Rom { get; set; }
-
-        /// <summary>
-        /// The path of the Program that was loaded.
-        /// </summary>
-        public string BiosFilePath { get; set; }
 
         /// <summary>
         /// The path of the Program that was loaded.
@@ -28,16 +18,6 @@
         /// <summary>
         /// The name of the Program that was loaded.
         /// </summary>
-        public string BiosFileName { get; set; }
-
-        /// <summary>
-        /// The name of the Program that was loaded.
-        /// </summary>
         public string RomFileName { get; set; }
-
-        /// <summary>
-        /// Tells the UI if the BIOS is to be tested by itself.
-        /// </summary>
-        public bool IsBiosOnly { get; set; }
     }
 }
