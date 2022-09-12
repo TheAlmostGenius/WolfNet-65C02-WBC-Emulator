3c9ac87dfc0881cf92e589560fede8c4f4eefbb4
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-09-12 14:08:43 +0100

    Updated documentation build system...

--- build-documentation.bat
+++ build-documentation.bat
@@ -1,13 +1,13 @@
 @echo off
 if exist .\Documentation (rd /S /Q .\Documentation)
 ::call InstallFont.bat
-Tools\Doxygen\doxygen.exe -w html NewHeader.htm NewFooter.htm Style.css
-Tools\Doxygen\doxygen.exe -w latex NewHeader.tex NewFooter.tex NewStylesheet.sty
+if not defined %%1 
+if %%1 == -u (Tools\Doxygen\doxygen.exe -w html NewHeader.htm NewFooter.htm Style.css && Tools\Doxygen\doxygen.exe -w latex NewHeader.tex NewFooter.tex NewStylesheet.sty)
 Tools\Doxygen\doxygen.exe doxyfile.config
 pushd .\Documentation\LaTeX\
 call make.bat
+call make.bat pdf
 popd
-move .\Documentation\LaTeX\refman.pdf .\Documentation\Documentation.pdf
 start .\Documentation\html\index.html
-start .\Documentation\Documentation.pdf
+if exist .\Documentation\LaTeX\refman.pdf (move .\Documentation\LaTeX\refman.pdf .\Documentation\Documentation.pdf && start .\Documentation\Documentation.pdf) else (echo Build failed: PDF output not found!)
 del *.bak
