5f4c7c55b5e0b9f3ec9879eacd50480705afe651
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-09-14 02:25:07 +0100

    Update build-documentation.bat

--- build-documentation.bat
+++ build-documentation.bat
@@ -9,4 +9,5 @@ start index.html
 start "C:\Program Files (x86)\HTML Help Workshop\hhw.exe" index.hhp
 popd
 if exist .\Documentation\LaTeX\refman.pdf (move .\Documentation\LaTeX\refman.pdf .\Documentation\Documentation.pdf && start .\Documentation\Documentation.pdf) else (echo Build failed: PDF output not found!)
+if exist .\Documentation\rtf\refman.rtf (move .\Documentation\rtf\refman.rtf .\Documentation\Documentation.rtf && start .\Documentation\Documentation.rtf) else (echo Build failed: RTF output not found!)
 del *.bak
