9919195edac6dcfbbccb16b364cfc05877df5882
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-09-20 23:25:14 +0100

    Revert "Revert "Bug fixes and support library updates...""
    This reverts commit 2bceeceeeff0a977a1efcf35f2c7d00f37a1db99.

--- Emulator/App.xaml.cs
+++ Emulator/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace Emulator
+﻿using System.Globalization;
+using System.Windows;
+
+namespace Emulator
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
 	public partial class App
-	{
+	{ 
 	}
 }
