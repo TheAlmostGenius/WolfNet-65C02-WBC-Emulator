b6fe796b49958904dbe43df2f2cd1623ac995330
John Wolfe <65527568+TheAlmostGenius@users.noreply.github.com>
2022-10-13 13:55:11 +0100

    Update GpioControl.xaml.cs

--- Emulator/GpioControl.xaml.cs
+++ Emulator/GpioControl.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using GalaSoft.MvvmLight.Messaging;
+using Hardware;
 using System.Windows;
 
 namespace Emulator
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for GpioControl.xaml
     /// </summary>
     public partial class GpioControl : Window
     {
+        W65C22 W65C22 { get; set; }
+
         public GpioControl()
         {
             InitializeComponent();
             Messenger.Default.Register<NotificationMessage>(this, NotificationMessageReceived);
+            Messenger.Default.Register<NotificationMessage<W65C22>>(this, NotificationMessageReceived);
         }
 
         private void NotificationMessageReceived(NotificationMessage notificationMessage)
@@ -23,6 +27,14 @@ namespace Emulator
             else if (notificationMessage.Notification == "CloseAll")
             {
                 Close();
+            }
+        }
+
+        private void NotificationMessageReceived(NotificationMessage<W65C22> notificationMessage)
+        {
+            if (notificationMessage.Notification == "GpioControlWindow")
+            {
+                W65C22 = notificationMessage.Content;
             }
         }
     }
