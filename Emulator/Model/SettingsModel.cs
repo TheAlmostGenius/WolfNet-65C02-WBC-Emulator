36958ebc00ac80fb9214dd8453f01db579c9f239
DarkestSoul1992 <65527568+DarkestSoul1992@users.noreply.github.com>
2023-05-02 12:11:40 +0100

    Update SettingsModel.cs

--- Emulator/Model/SettingsModel.cs
+++ Emulator/Model/SettingsModel.cs
@@ -4,34 +4,34 @@ using System.Xml.Serialization;
 namespace Emulator.Model
 {
     /// <summary>
-    /// Model that contains the required information needed to save the current settings to disk
+    /// Model that contains the required information needed to save the current settings to disk.
     /// </summary>
     [Serializable]
     [XmlRootAttribute("SettingsFileModel", Namespace = "Emulator.Model", IsNullable = false)]
     public class SettingsModel
     {
         /// <summary>
-        /// The version of the file that is being saved
+        /// The version of the file that is being saved.
         /// </summary>
         public byte SettingsVersionMajor { get; set; }
 
         /// <summary>
-        /// The version of the file that is being saved
+        /// The version of the file that is being saved.
         /// </summary>
         public byte SettingsVersionMinor { get; set; }
 
         /// <summary>
-        /// The version of the file that is being saved
+        /// The version of the file that is being saved.
         /// </summary>
         public byte SettingsVersionBuild { get; set; }
 
         /// <summary>
-        /// The version of the file that is being saved
+        /// The version of the file that is being saved.
         /// </summary>
         public byte SettingsVersionRevision { get; set; }
 
         /// <summary>
-        /// The PC port that is being saved
+        /// The PC port that is being saved.
         /// </summary>
         public string ComPortName { get; set; }
     }
